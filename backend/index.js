baf5d116eaa11fe75c823b98dfbd140320add0cf
Bùi Văn Nhật <nhatdev195@gmail.com>
2025-10-15 05:45:36 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ const corsOptions = {
   methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
 };
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+
 
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
