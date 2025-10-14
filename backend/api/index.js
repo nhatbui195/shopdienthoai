aa5caa5740369babe42ff370437624b5f49433a1
nhatbui195 <nhatdev195@gmail.com>
2025-10-15 03:14:00 +0700

    ignore env files

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,9 +1,3 @@
-// backend/api/index.js
 const serverless = require('serverless-http');
-const app = require('../index');
-
-module.exports = (req, res) => {
-  // có thể set header chung ở đây nếu cần
-  const handler = serverless(app);
-  return handler(req, res);
-};
+const app = require('../index'); // chính là express() ở trên
+module.exports = serverless(app);
